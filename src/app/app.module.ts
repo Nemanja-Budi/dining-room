2ccf3330cdfc1919a145b43b6750d82ea3a96973
Nemanja Budimirovic <96budi@gmail.com>
2024-02-28 12:24:57 +0100

    Dodavanje komponenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RestoranListComponent } from './components/core/restoran-list/restoran-list.component';
+import { HomeComponent } from './components/core/home/home.component';
+import { AboutComponent } from './components/core/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    RestoranListComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/core/restoran-list/restoran-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restoran-list',
+  templateUrl: './restoran-list.component.html',
+  styleUrls: ['./restoran-list.component.css']
+})
+export class RestoranListComponent {
+
+}
