932eb27edbf79fd5b2cbbaa0df151f6feba1b801
Nemanja Budimirovic <96budi@gmail.com>
2024-02-28 12:28:39 +0100

    Dodavanje statickih ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/core/home/home.component';
+import { AboutComponent } from './components/core/about/about.component';
+import { RestoranListComponent } from './components/core/restoran-list/restoran-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'restaurants', component: RestoranListComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'prefix'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
